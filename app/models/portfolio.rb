3d3b5942392c39516fcd5f13abfc2e3dc93c23fe
Michael Insalaco <insalaco01@gmail.com>
2018-09-30 06:04:32 -0400

    add nested attributes for technologies

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,9 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
   validates_presence_of :title, :body, :main_image, :thumb_image
+  
+  accepts_nested_attributes_for :technologies, 
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle = "Ruby on Rails") }
 
