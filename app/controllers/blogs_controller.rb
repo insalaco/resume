c7539795d0157e1f1e52803aa43eaf8439cc3e6e
Michael Insalaco <insalaco01@gmail.com>
2018-10-02 18:47:43 -0400

    add navbar links and default page content concern

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,6 +5,7 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     @blogs = Blog.all
+    @page_title = "My Portfolio Blog | Michael Insalaco"
   end
 
   # GET /blogs/1
