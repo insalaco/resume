13fd868f06a26a2e8f9ecaeeb751e4cea35b71e2
Michael Insalaco <insalaco01@gmail.com>
2018-10-11 18:00:20 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 3.times do |topic|
-  Topic.create!(title: "#{topic} Topic Name"
+  Topic.create!(
+    title: "Topic #{topic}"
   )
 end
 
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
-    title: "My blog post #{blog}",
-    body: "Contrary to popular belief, Lorem Ipsum is not 
-    simply random text. It has roots in a piece of classical 
-    Latin literature from 45 BC, making it over 2000 years old. 
-    Richard McClintock, a Latin professor at Hampden-Sydney 
-    College in Virginia, looked up one of the more obscure 
-    Latin words",
+    title: "My Blog Post #{blog}",
+    body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
     topic_id: Topic.last.id
   )
 end
 
+puts "10 blog posts created"
+
 5.times do |skill|
   Skill.create!(
-  title: "Rails #{skill}",
-  percent: 15
+    title: "Rails #{skill}",
+    percent: 15
   )
 end
 
-9.times do |portfolio_item|
+puts "5 skills created"
+
+8.times do |portfolio_item|
   Portfolio.create!(
-    title: "Portfolio Title #{portfolio_item}",
+    title: "Portfolio title: #{portfolio_item}",
     subtitle: "Ruby on Rails",
-    body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
-    main_image: "https://via.placeholder.com/600x400",
-    thumb_image: "https://via.placeholder.com/350x200"
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
   )
 end
 
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+puts "9 portfolio items created"
 
 3.times do |technology|
-  Portfolio.last.technologies.create(
-    name: "Technology #{technology}"     
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
   )
 end
+
+puts "3 technologies created"
