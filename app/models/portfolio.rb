82c32c56b516c2d3bd08e59b6e0fa494d982b40e
Michael Insalaco <insalaco01@gmail.com>
2018-10-11 18:00:10 -0400

    remove validations for images to prevent seed error

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,24 +1,20 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
-  validates_presence_of :title, :body, :main_image, :thumb_image
+
+  accepts_nested_attributes_for :technologies, 
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
+
+  validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
   
-  accepts_nested_attributes_for :technologies, 
-                                reject_if: lambda { |attrs| attrs['name'].blank? }
+  
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle = "Ruby on Rails") }
   
   def self.by_position
     order("position ASC")
   end
-  
-  after_initialize :set_defaults
 
-  def set_defaults
-    # set image defaults if images are nil
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-  end
 end
